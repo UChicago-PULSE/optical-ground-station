d747cf30d362e1365ab27dd26b2d2ec991152b2e
Juan Prieto <jiprieto@uchicago.edu>
2025-02-16 02:29:58 -0600

    Updated class. Still need to make some more methods, but most of it is done

--- Polarimeter Data Collection/CSV collection.py
+++ Polarimeter Data Collection/CSV collection.py
@@ -1,5 +1,7 @@
-#from Pol_Measurement_Class import Pol_Measurement
+from Pol_Measurement_Class import Pol_Measurement
 
 file_path = r"C:\Users\juani\Personal\CubeSat\Polarimeter Data\Sample_polarimeter.csv"
-#measurement_1 = Pol_Measurement("Measure 1", file_path)
+measurement_1 = Pol_Measurement("Measure 1", file_path)
 
+print(measurement_1.data)
+print(measurement_1.time_to_milliseconds())
--- Polarimeter Data Collection/Pol_Measurement_Class.py
+++ Polarimeter Data Collection/Pol_Measurement_Class.py
@@ -1,9 +1,10 @@
 class Pol_Measurement:
     """
     Class that can be used to retrieve attributes and stuff from multiple measurements with the
-    polarimeter. Seems like a good idea to me lol.
+    polarimeter. Seems like a good idea to me lol. The idea is to reorganize all the data from the
+    csv files into attributes, and then create methods useful for data analysis.
     """
-    # Initialize measurement
+    # Initialize measurement.
     def __init__(self, name: str, file_path: str):
         self.name = name
         self.path = file_path
@@ -31,6 +32,31 @@ class Pol_Measurement:
         self.settings = settings_dict
         self.sample_rate = self.settings["Basic Sample Rate [Hz]"]
         self.wavelength = self.settings["Wavelength [nm]"]
+        # Creating data dictionary:
+        data_dict = {}
+        # Creating key values for data dict (from row 9 in csv file)
+        key_vals = file_content_lines[8].split("; ")
+        # Creating data dictionary
+        data_dict = {}
+        # -1 comes from the final column being "warnings", which are empty
+        for x in range(len(key_vals) - 1):
+            data_dict[key_vals[x]] = []
         # Getting the data out into a dictionary of lists of data:
         # -9 since the data doesn't start until line 10
-        for n in range(len(file_content_lines)-9):
+        for n in range(len(file_content_lines) - 9):
+            data_list = file_content_lines[n + 9].split(";")
+            # Again, empty warning column is avoided with -1, as it results in a list of ","s
+            for m in range(len(data_list) - 1):
+                if m > 1:
+                    data_dict[key_vals[m]] += [float(data_list[m])]
+                else:
+                    data_dict[key_vals[m]] += [data_list[m]]
+        self.data = data_dict
+
+    def time_to_milliseconds(self) -> list[float]:
+        time_list = self.data["Elapsed Time [hh:mm:ss:ms]"]
+        time_ms_list = []
+        for i in range(len(time_list)):
+            split_time = time_list[i].split(":")
+            time_ms_list.append(3600000 * float(split_time[0]) + 60000 * float(split_time[1]) + 1000 * float(split_time[2]) + float(split_time[3]))
+        return time_ms_list
