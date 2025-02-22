057b067ba3dd3befbd811feb5eda1cca2226dc61
Juan Prieto <jiprieto@uchicago.edu>
2025-02-22 17:36:41 -0600

    Added function to read multiple files from single directory

--- Polarimeter Data Collection/CSV collection.py
+++ Polarimeter Data Collection/CSV collection.py
@@ -1,18 +1,37 @@
 from Pol_Measurement_Class import Pol_Measurement
+import os
 
-file_path = r"C:\Users\juani\Personal\CubeSat\Polarimeter Data\Sample_polarimeter.csv"
-measurement_1 = Pol_Measurement("Measure 1", file_path)
+#file_path = r"C:\Users\juani\Personal\CubeSat\Polarimeter Data\Sample_polarimeter.csv"
+#measurement_1 = Pol_Measurement("Measure 1", file_path)
 
-#print(measurement_1.data)
-#print(measurement_1.time_to_milliseconds())
-#print(measurement_1.average("S 0 [mW]"))
-#print(measurement_1.average("Hello"))
-#print(measurement_1.stdev("S 0 [mW]"))
+def create_file_paths_non_recursive(dir_path: str) -> list[str]:
+    """
+    :param dir_path: path of directory as a string
+    :return: returns file paths inside of the directory
+    """
+    if not os.path.exists(dir_path) or not os.path.isdir(dir_path):
+        raise ValueError("The provided path is not a valid directory.")
+    else:
+        file_paths = [os.path.join(dir_path, file_name) for file_name in os.listdir(dir_path) if
+                      os.path.isfile(os.path.join(dir_path, file_name))]
+        return file_paths
 
-key_list = measurement_1.data_keys
-print(key_list)
+def create_file_paths_recursive(dir_path: str, file_paths: list) -> list[str]:
+    """
+    :param dir_path: path of directory as a string
+    :return: returns file paths inside of the directory. Works for a directory of directories
+    """
+    if os.path.isfile(dir_path):
+        file_paths.append(dir_path)
+        return file_paths
+    elif os.path.isdir(dir_path):
+        for file_name in os.listdir(dir_path):
+            file_paths = create_file_paths_recursive(os.path.join(dir_path, file_name), file_paths)
+        return file_paths
 
-for i in range(len(key_list)):
-    print(key_list[i])
-    print(measurement_1.average(key_list[i]))
-    print(measurement_1.stdev(key_list[i]))
+def create_measurement_objects(file_paths: list[str]):
+    """
+    :param file_paths: list of file paths as strings
+    :return: returns a list of polarization measurement objects
+    """
+    raise NotImplemented
