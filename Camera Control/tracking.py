3ae46ff44e27fd26328c7c02998cf1a556189989
Dillon Bass <109323420+dlllonb@users.noreply.github.com>
2025-03-27 13:04:09 -0500

    Update tracking.py

--- Camera Control/tracking.py
+++ Camera Control/tracking.py
@@ -1,1 +1,1 @@
-# file that does the tracking loop, takes a quick image, finds the satellite signal, and tells the telescope to correct position
+# file that does the tracking loop, takes a quick image, finds the satellite signal, and gives a "slew rate vector correction"
