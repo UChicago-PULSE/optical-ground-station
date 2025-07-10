6aa0479e3339922135cc5575d60027bb61f116c5
Ashley Ashiku <ashleyashiku@ashleys-MacBook-Pro.local>
2025-07-09 22:32:11 -0700

    Commented out camera code

--- Camera Control/cameraconnect.py
+++ Camera Control/cameraconnect.py
@@ -1,23 +1,37 @@
-# imports needed
+"""
+The purpose of this code is to connect to a ZWO ASI camera.
+
+This code was compiled by Ashley Ashiku, telescope engineer, for the PULSE-A project. This code utilizes Python wrappers written by Steve Marple, 2017 
+(search python-zwoasi on GitHub) around C commands for ZWO ASI cameras from the software development kit.
+
+Last updated July 9th, 2025
+
+Correspondence/Questions:
+aashiku@uchicago.edu
+"""
+
+# Imports needed 
 import zwoasi as asi
 import sys
 import os
 from astropy.io import fits
 import time
 
-# suppress this weird error message
+# My attempt to suppress the weird error message "WARNING:root:ASI SDK library not found"
+# It didn't work, so ignore this:
 #import logging
 #logging.getLogger().setLevel(logging.CRITICAL)
 
-# first load ZWOASI SDK (library of C commands)
+# Load ZWOASI SDK, the library of C commands that you downloaded. Replace the filepath to suite your own computer!
 sdk_filename = '/Users/ashleyashiku/Desktop/PULSE-A/ASI_Camera_SDK/ASI_linux_mac_SDK_V1.37/lib/mac/libASICamera2.dylib'
 asi.init(sdk_filename)
 
+# Manually plug the ZWO camera into your own laptop using a USB connector and any converters necessary.
 
-# Now we're connecting to camera.
+# This returns the number of connected cameras
 print(f"Number of connected cameras: {asi.get_num_cameras()}")
 
-#list cameras and connect to first
+# This lists the cameras connected and automatically connects to the first, since we are only connecting one
 num_cameras = asi.get_num_cameras()
 if num_cameras == 0:
     print('No cameras found')
@@ -36,16 +50,28 @@ else:
     camera_id = 0
     print('Using #%d: %s' % (camera_id, cameras_found[camera_id]))
 
+# Establishing which camera we're using
 camera = asi.Camera(camera_id)
 
+# You're now connected to the ZWO camera! Next, we're printing out various camera properties so you see what we're working with
 print("Getting camera property.")
 cam_prop = camera.get_camera_property()
+
+# I am most interested in gain and pixel size, so that's what I printed out specifically. You can specify any specific properties you want, or
+# alternatively, just print all of them with:
+#print(cam_prop)
 print(f"Gain: {cam_prop['ElecPerADU']:2f}")
 print(f"Pixel size: {cam_prop['PixelSize']}")
 
-camera.set_control_value(asi.ASI_EXPOSURE, 15000000)
+# You can also change certain settings if you please. I wrote a function in camerapicture.py to do that, so no need to do so now
+# But use the following code if you want to now:
+camera.set_control_value(asi.ASI_EXPOSURE, 15000000) # Here, keep in mind that the exposure time is input in microseconds (10 ^ -6 seconds)
 
-#
+
+
+# Great, that's it! Ignore everything below, this is code I was messing around with.
+
+
 """
 print('Capturing a single 8-bit mono image')
 
@@ -64,14 +90,14 @@ fits.writeto(filename, image, overwrite=True)
 
 print(f"image should now be saved in {filename}. go look!")
 """
-camera.stop_video_capture()
+#camera.stop_video_capture()
 
-print('Enabling video mode')
+#print('Enabling video mode')
 
-time.sleep(5)
-camera.stop_video_capture()
-print("ended cam")
-chat = camera.get_video_data()
+#time.sleep(5)
+#camera.stop_video_capture()
+#print("ended cam")
+#chat = camera.get_video_data()
 
 #if __name__ == "__main__":
   #  camconnect()
